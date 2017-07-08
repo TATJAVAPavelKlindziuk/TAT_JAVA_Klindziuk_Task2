b9618bf1bab7cab4f2b7a6588fc9c4372eccd8b8
Pavel Klindziuk <pavel_klindziuk@epam.com>
2017-07-08 14:06:34 +0300

    parametrized tests

--- src/test/java/com/klindziuk/offlinelibrary/controller/test/UpdateDescription.java
+++ src/test/java/com/klindziuk/offlinelibrary/controller/test/UpdateDescription.java
@@ -3,89 +3,64 @@ package com.klindziuk.offlinelibrary.controller.test;
 import java.io.IOException;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.klindziuk.offlinelibrary.controller.util.RequestParser;
 
 public class UpdateDescription extends BaseTest {
+	private static final String UPDATEDECRIPTION_XML_PATH = XMLFILEPATH + "adminservice/updatebookdescription/";
 	
-	@Test(priority = 1)
+	@Test(priority = 0)
 	public void updateDescriptionSmokeTest() throws IOException {
-		request = RequestParser.readFile(XMLFILEPATH + "adminservice/updatedescription/updatebookdescription.xml");
+		request = RequestParser.readFile(UPDATEDECRIPTION_XML_PATH + "updatebookdescription.xml");
 		String actual = controller.executeAdminTask(request);
 		String expected = "Book description successfully updated.";
 		Assert.assertEquals(actual, expected);
 	}
-
-	@Test(priority = 2)
-	public void updateDescriptionEmptyNameTest() throws IOException {
-		request = RequestParser.readFile(XMLFILEPATH + "adminservice/updatedescription/emptyname.xml");
-		String actual = controller.executeAdminTask(request);
-		String expected = "Cannot perform this operation.Parameters cannot be null or empty.";
-		Assert.assertEquals(actual, expected);
+		
+	@DataProvider
+	public Object[][] updateDescriptionIdDp() throws IOException {
+		return new Object[][] { 
+			    { UPDATEDECRIPTION_XML_PATH + "idempty.xml", EMPTY_STRING_EXCEPTION_MESSAGE },
+				{ UPDATEDECRIPTION_XML_PATH + "idletters.xml", NUMBER_EXCEPTION_MESSAGE },
+				{ UPDATEDECRIPTION_XML_PATH + "idspec.xml", NUMBER_EXCEPTION_MESSAGE }, };
 	}
 
-	@Test(priority = 3)
-	public void updateDescriptionNumbersNameTest() throws IOException {
-		request = RequestParser.readFile(XMLFILEPATH + "adminservice/updatedescription/numbersname.xml");
-		String actual = controller.executeAdminTask(request);
-		String expected = "Cannot perform this operation.Only letters,dots,minus and whitespaces are allowed.";
-		Assert.assertEquals(actual, expected);
-	}
-
-	@Test(priority = 4)
-	public void updateDescriptionSpecNameTest() throws IOException {
-		request = RequestParser.readFile(XMLFILEPATH + "adminservice/updatedescription/specname.xml");
-		String actual = controller.executeAdminTask(request);
-		String expected = "Cannot perform this operation.Only letters,dots,minus and whitespaces are allowed.";
+	@Test(priority = 1, dataProvider = "updateDescriptionIdDp")
+	public void updateDescriptionIdTest(String request, String expected) throws IOException {
+		String command = RequestParser.readFile(request);
+		String actual = controller.executeAdminTask(command);
 		Assert.assertEquals(actual, expected);
 	}
 	
-	@Test(priority = 5)
-	public void updateDescriptionEmptyAuthorTest() throws IOException {
-		request = RequestParser.readFile(XMLFILEPATH + "adminservice/updatedescription/emptyauthor.xml");
-		String actual = controller.executeAdminTask(request);
-		String expected = "Cannot perform this operation.Parameters cannot be null or empty.";
-		Assert.assertEquals(actual, expected);
+	@DataProvider
+	public Object[][] updateDescriptionNameDp() throws IOException {
+		return new Object[][] { 
+			    { UPDATEDECRIPTION_XML_PATH + "nameempty.xml", EMPTY_STRING_EXCEPTION_MESSAGE },
+				{ UPDATEDECRIPTION_XML_PATH + "namenumbers.xml", SPEC_STRING_EXCEPTION_MESSAGE },
+				{ UPDATEDECRIPTION_XML_PATH + "namespec.xml", SPEC_STRING_EXCEPTION_MESSAGE }, };
 	}
 
-	@Test(priority = 6)
-	public void updateDescriptionNumbersAuthorTest() throws IOException {
-		request = RequestParser.readFile(XMLFILEPATH + "adminservice/updatedescription/numbersauthor.xml");
-		String actual = controller.executeAdminTask(request);
-		String expected = "Cannot perform this operation.Only letters,dots,minus and whitespaces are allowed.";
-		Assert.assertEquals(actual, expected);
-	}
-
-	@Test(priority = 7)
-	public void updateDescriptionSpecAuthorTest() throws IOException {
-		request = RequestParser.readFile(XMLFILEPATH + "adminservice/updatedescription/specauthor.xml");
-		String actual = controller.executeAdminTask(request);
-		String expected = "Cannot perform this operation.Only letters,dots,minus and whitespaces are allowed.";
+	@Test(priority = 2, dataProvider = "updateDescriptionNameDp")
+	public void updateDescriptionNameTest(String request, String expected) throws IOException {
+		String command = RequestParser.readFile(request);
+		String actual = controller.executeAdminTask(command);
 		Assert.assertEquals(actual, expected);
 	}
 	
-	@Test(priority = 8)
-	public void updateDescriptionEmptyIdTest() throws IOException {
-		request = RequestParser.readFile(XMLFILEPATH + "adminservice/updatedescription/emptyid.xml");
-		String actual = controller.executeAdminTask(request);
-		String expected = "Cannot perform this operation.Parameters cannot be null or empty.";
-		Assert.assertEquals(actual, expected);
+	@DataProvider
+	public Object[][] updateDescriptionAuthorDp() throws IOException {
+		return new Object[][] { 
+			    { UPDATEDECRIPTION_XML_PATH + "authorempty.xml", EMPTY_STRING_EXCEPTION_MESSAGE },
+				{ UPDATEDECRIPTION_XML_PATH + "authornumbers.xml", SPEC_STRING_EXCEPTION_MESSAGE },
+				{ UPDATEDECRIPTION_XML_PATH + "authorspec.xml", SPEC_STRING_EXCEPTION_MESSAGE }, };
 	}
 
-	@Test(priority = 9)
-	public void updateDescriptionLettersIdTest() throws IOException {
-		request = RequestParser.readFile(XMLFILEPATH + "adminservice/updatedescription/lettersId.xml");
-		String actual = controller.executeAdminTask(request);
-		String expected = "Cannot perform this operation.Only numbers allowed.Id cannot be zero.";
-		Assert.assertEquals(actual, expected);
-	}
-
-	@Test(priority = 10)
-	public void updateDescriptionSpecIdTest() throws IOException {
-		request = RequestParser.readFile(XMLFILEPATH + "adminservice/updatedescription/specId.xml");
-		String actual = controller.executeAdminTask(request);
-		String expected = "Cannot perform this operation.Only numbers allowed.Id cannot be zero.";
+	@Test(priority = 3, dataProvider = "updateDescriptionAuthorDp")
+	public void addBookEmptyFieldsTest(String request, String expected) throws IOException {
+		String command = RequestParser.readFile(request);
+		String actual = controller.executeAdminTask(command);
 		Assert.assertEquals(actual, expected);
 	}
 }
