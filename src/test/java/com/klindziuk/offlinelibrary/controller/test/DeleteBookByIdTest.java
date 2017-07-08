a220c09520f372b3f34e1d6f32f9c40bcc98b846
Pavel Klindziuk <pavel_klindziuk@epam.com>
2017-07-08 14:44:59 +0300

    parametrized tests del

--- src/test/java/com/klindziuk/offlinelibrary/controller/test/DeleteBookByIdTest.java
+++ src/test/java/com/klindziuk/offlinelibrary/controller/test/DeleteBookByIdTest.java
@@ -3,49 +3,34 @@ package com.klindziuk.offlinelibrary.controller.test;
 import java.io.IOException;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.klindziuk.offlinelibrary.controller.util.RequestParser;
 
 public class DeleteBookByIdTest extends BaseTest {
-	
+	private static final String DELETE_XML_PATH = XMLFILEPATH + "adminservice/deletebookbyid/";
+
 	@Test(priority = 1)
 	public void deleteBookByIdSmokeTest() throws IOException {
-		request = RequestParser.readFile(XMLFILEPATH + "adminservice/deletebookbyid/deletebookbyid.xml");
+		request = RequestParser.readFile(DELETE_XML_PATH + "deletebookbyid.xml");
 		String actual = controller.executeAdminTask(request);
 		String expected = "Book deleted successfully.";
 		Assert.assertEquals(actual, expected);
 	}
-	
-	@Test(priority = 2)
-	public void deleteBookByIdEmptyIdTest() throws IOException {
-		request = RequestParser.readFile(XMLFILEPATH + "adminservice/deletebookbyid/deletebookbyidempty.xml");
-		String actual = controller.executeAdminTask(request);
-		String expected = "Cannot perform this operation.Parameters cannot be null or empty.";
-		Assert.assertEquals(actual, expected);
+
+	@DataProvider
+	public Object[][] deleteBookByIdDp() throws IOException {
+		return new Object[][] { { DELETE_XML_PATH + "empty.xml", EMPTY_STRING_EXCEPTION_MESSAGE },
+				{ DELETE_XML_PATH + "zero.xml", NUMBER_EXCEPTION_MESSAGE },
+				{ DELETE_XML_PATH + "letters.xml", NUMBER_EXCEPTION_MESSAGE },
+				{ DELETE_XML_PATH + "spec.xml", NUMBER_EXCEPTION_MESSAGE }, };
 	}
 
-	@Test(priority = 3)
-	public void deleteBookByIdLettersIdTest() throws IOException {
-		request = RequestParser.readFile(XMLFILEPATH + "adminservice/deletebookbyid/deletebookbyidletters.xml");
-		String actual = controller.executeAdminTask(request);
-		String expected = "Cannot perform this operation.Only numbers allowed.Id cannot be zero.";
-		Assert.assertEquals(actual, expected);
-	}
-
-	@Test(priority = 4)
-	public void deleteBookByIdZeroIdTest() throws IOException {
-		request = RequestParser.readFile(XMLFILEPATH + "adminservice/deletebookbyid/deletebookbyidzero.xml");
-		String actual = controller.executeAdminTask(request);
-		String expected = "Cannot perform this operation.Only numbers allowed.Id cannot be zero.";
-		Assert.assertEquals(actual, expected);
-	}
-
-	@Test(priority = 5)
-	public void deleteBookByIdSpecIdTest() throws IOException {
-		request = RequestParser.readFile(XMLFILEPATH + "adminservice/deletebookbyid/deletebookbyidspec.xml");
-		String actual = controller.executeAdminTask(request);
-		String expected = "Cannot perform this operation.Only numbers allowed.Id cannot be zero.";
+	@Test(priority = 2, dataProvider = "deleteBookByIdDp")
+	public void deleteBookByIdTest(String request, String expected) throws IOException {
+		String command = RequestParser.readFile(request);
+		String actual = controller.executeAdminTask(command);
 		Assert.assertEquals(actual, expected);
 	}
 }
